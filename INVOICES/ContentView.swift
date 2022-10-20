a1e5a1835d26e7cdc92076da09f0c9a072d0a282
Koen Sas <koen@ivision.be>
2022-10-20 20:05:14 +0200

    Initial Commit

---
+++ INVOICES/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  INVOICES
+//
+//  Created by Koen Sas on 20/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPad Air (5th generation)")
+            .previewDevice("iPhone X")
+    }
+}
---
+++ INVOICES/INVOICESApp.swift
@@ -0,0 +1,17 @@
+//
+//  INVOICESApp.swift
+//  INVOICES
+//
+//  Created by Koen Sas on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct INVOICESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
